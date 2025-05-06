17ef2f6e4c770042842e388e09f0b654ad8bb5b1
Gardian <gardianrahman91@gmail.com>
2025-05-06 16:10:46 +0700

    first commit

---
+++ PointerdanFriendClass_0083.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class PersegiPanjang
+{
+    public:
+        int panjang, lebar;
+
+    public:
+        void input()
+        {
+            cout << "Masukkan panjang : ";
+            cin >> panjang;
+            cout << "Masukan Lebar : ";
+            cin >> lebar;
+        }
+
+        int luasPp(int a, int b)
+        {
+            return a * b;
+        }
+        
+};
+
+class Lingkaran
+{
+    public:
+        int jarijari;
+
+        void input()
+        {
+            cout<<"Masukan Jari-Jari : ";
+            cin >> jarijari;
+        }
+
+        int luasLingkaran (int r)
+        {
+            return 3.14 * r * r;
+        }
+};
+
